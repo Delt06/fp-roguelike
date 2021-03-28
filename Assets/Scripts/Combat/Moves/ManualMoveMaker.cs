5f9bbdc473d53cc6239bfa41639f6b2b32c35e36
Vladislav Kantaev <vvkantaev@yandex.ru>
2021-03-28 21:58:25 +0300

    Add manual move making

--- Assets/Scripts/Combat/Moves/ManualMoveMaker.cs
+++ Assets/Scripts/Combat/Moves/ManualMoveMaker.cs
@@ -1,21 +1,44 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Combat.Moves
 {
 	public sealed class ManualMoveMaker : MonoBehaviour, ICombatMoveMaker
 	{
-		public Move? Move { get; set; }
+		public Move? PendingMove { get; set; }
+
+		public bool DemandMove
+		{
+			get => _demandMove;
+			private set
+			{
+				if (_demandMove == value) return;
+				_demandMove = value;
+				if (_demandMove)
+					MoveDemandBegan?.Invoke();
+				else
+					MoveDemandEnded?.Invoke();
+			}
+		}
+
+		public event Action MoveDemandBegan;
+		public event Action MoveDemandEnded;
 
 		public bool TryMakeMove(out Move move)
 		{
-			if (Move.HasValue)
+			if (PendingMove.HasValue)
 			{
-				move = Move.Value;
+				move = PendingMove.Value;
+				PendingMove = null;
+				DemandMove = false;
 				return true;
 			}
 
+			DemandMove = true;
 			move = default;
 			return false;
 		}
+
+		private bool _demandMove;
 	}
 }
---
+++ Assets/Scripts/UI/MoveMakingScreen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Combat.Moves;
+using Combat.Moves.Presets;
+using DELTation.Entities;
+using UnityEngine;
+
+namespace UI
+{
+	public sealed class MoveMakingScreen : MonoBehaviour
+	{
+		[SerializeField] private GameObject _content = default;
+		[SerializeField] private EntityBase _entity = default;
+
+		public void OnClick(MovePreset move)
+		{
+			MoveMaker.PendingMove = move.GetMove(_entity);
+		}
+
+		private void OnEnable()
+		{
+			MoveMaker.MoveDemandBegan += _refresh;
+			MoveMaker.MoveDemandEnded += _refresh;
+		}
+
+		private void OnDisable()
+		{
+			MoveMaker.MoveDemandBegan -= _refresh;
+			MoveMaker.MoveDemandEnded -= _refresh;
+		}
+
+		private void Awake()
+		{
+			_refresh = Refresh;
+		}
+
+		private void Refresh()
+		{
+			_content.SetActive(MoveMaker.DemandMove);
+		}
+
+		private ManualMoveMaker MoveMaker => _entity.Get<ManualMoveMaker>();
+		
+		private Action _refresh;
+	}
+}
