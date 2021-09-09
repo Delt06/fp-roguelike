a8e58ab8529958cecfb9e553caab7fd3bb4afd89
Vladislav Kantaev <vvkantaev@yandex.ru>
2021-09-09 22:44:31 +0300

    Change emit project files method

--- Assets/Plugins/Editor/BuildUtilities.cs
+++ Assets/Plugins/Editor/BuildUtilities.cs
@@ -1,12 +1,23 @@
-﻿using UnityEditor;
+﻿using System;
+using System.Reflection;
+using JetBrains.Annotations;
 
 namespace Plugins.Editor
 {
 	public static class BuildUtilities
 	{
+		[UsedImplicitly]
 		public static void EmitProjectFiles()
 		{
-			EditorApplication.ExecuteMenuItem("Assets/Open C# Project");
+			var T = Type.GetType("UnityEditor.SyncVS,UnityEditor");
+			if (T == null)
+				throw new InvalidOperationException("Type not found.");
+
+			var syncSolution = T.GetMethod("SyncSolution", BindingFlags.Public | BindingFlags.Static);
+			if (syncSolution == null)
+				throw new InvalidOperationException("Method not found.");
+
+			syncSolution.Invoke(null, null);
 		}
 	}
 }
